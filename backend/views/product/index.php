5079a91806b0d4fa671c43c7789a2db6cf9e8544
LAPTOP-F8PG4713\Suyash <suyashcr07@gmail.com>
2022-05-11 12:22:33 +0530

    Improve product list

--- backend/views/product/index.php
+++ backend/views/product/index.php
@@ -14,38 +14,69 @@ $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="product-index">
 
-    <h1><?= Html::encode($this->title) ?></h1>
+	<h1><?= Html::encode($this->title) ?></h1>
 
-    <p>
-        <?= Html::a('Create Product', ['create'], ['class' => 'btn btn-success']) ?>
-    </p>
+	<p>
+		<?= Html::a('Create Product', ['create'], ['class' => 'btn btn-success']) ?>
+	</p>
 
-    <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
+	<?php // echo $this->render('_search', ['model' => $searchModel]); ?>
 
-    <?= GridView::widget([
-        'dataProvider' => $dataProvider,
-        'filterModel' => $searchModel,
-        'columns' => [
-            ['class' => 'yii\grid\SerialColumn'],
-
-            'id',
-            'name',
-            'description:ntext',
-            'image',
-            'price',
-            //'status',
-            //'created_at',
-            //'updated_at',
-            //'created_by',
-            //'updated_by',
-            [
-                'class' => ActionColumn::class,
-                'urlCreator' => function ($action, \common\models\Product $model, $key, $index, $column) {
-                    return Url::toRoute([$action, 'id' => $model->id]);
-                 }
-            ],
-        ],
-    ]); ?>
+	<?= GridView::widget([
+		'dataProvider' => $dataProvider,
+		'filterModel' => $searchModel,
+		'columns' => [
+			[
+				'attribute' => 'id',
+				'contentOptions' => [
+					'style' => 'width: 80px;'
+				]
+			],
+			[
+				'label' => 'Image',
+				'attribute' => 'image',
+				'content' => function ($model) {
+					/** @var $model \common\models\Product */
+					return Html::img($model->getImageUrl(), ['style' => 'width: 50px']);
+				}
+			],
+			'name',
+			'image',
+			'price:currency',
+			[
+				'attribute' => 'status',
+				'content' => function ($model) {
+					/** @var $model \common\models\Product */
+					return Html::tag(
+						'span', $model->status ? 'Active' : 'Draft',
+						['class' => $model->status ? 'badge badge-success' : 'badge badge-danger']
+					);
+				}
+			],
+			[
+				'attribute' => 'created_at',
+				'format' => ['datetime'],
+				'contentOptions' => [
+					'style' => 'white-space: nowrap'
+				]
+			],
+			[
+				'attribute' => 'updated_at',
+				'format' => ['datetime'],
+				'contentOptions' => [
+					'style' => 'white-space: nowrap'
+				]
+			],
+			//'created_by',
+			//'updated_by',
+			[
+				'class' => ActionColumn::class,
+				'urlCreator' => function ($action, \common\models\Product $model, $key, $index, $column) {
+					return Url::toRoute([$action, 'id' => $model->id]);
+				}
+			],
+		],
+	]); ?>
 
 
 </div>

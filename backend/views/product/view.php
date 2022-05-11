e65fb7089880090c1f449c417b0951d5dc7dac90
LAPTOP-F8PG4713\Suyash <suyashcr07@gmail.com>
2022-05-11 13:13:44 +0530

    Customise product view on backend

--- backend/views/product/view.php
+++ backend/views/product/view.php
@@ -13,33 +13,44 @@ $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="product-view">
 
-    <h1><?= Html::encode($this->title) ?></h1>
+	<h1><?= Html::encode($this->title) ?></h1>
 
-    <p>
-        <?= Html::a('Update', ['update', 'id' => $model->id], ['class' => 'btn btn-primary']) ?>
-        <?= Html::a('Delete', ['delete', 'id' => $model->id], [
-            'class' => 'btn btn-danger',
-            'data' => [
-                'confirm' => 'Are you sure you want to delete this item?',
-                'method' => 'post',
-            ],
-        ]) ?>
-    </p>
+	<p>
+		<?= Html::a('Update', ['update', 'id' => $model->id], ['class' => 'btn btn-primary']) ?>
+		<?= Html::a('Delete', ['delete', 'id' => $model->id], [
+			'class' => 'btn btn-danger',
+			'data' => [
+				'confirm' => 'Are you sure you want to delete this item?',
+				'method' => 'post',
+			],
+		]) ?>
+	</p>
 
-    <?= DetailView::widget([
-        'model' => $model,
-        'attributes' => [
-            'id',
-            'name',
-            'description:ntext',
-            'image',
-            'price',
-            'status',
-            'created_at',
-            'updated_at',
-            'created_by',
-            'updated_by',
-        ],
-    ]) ?>
+	<?= DetailView::widget([
+		'model' => $model,
+		'attributes' => [
+			'id',
+			[
+				'attribute' => 'image',
+				'format' => 'html',
+				'value' => fn() => Html::img($model->getImageUrl(), ['style' => 'width: 50px'])
+			],
+			'name',
+			'description:html',
+			'price:currency',
+			[
+				'attribute' => 'status',
+				'format' => 'html',
+				'value' => fn() => Html::tag(
+					'span', $model->status ? 'Active' : 'Draft',
+					['class' => $model->status ? 'badge badge-success' : 'badge badge-danger']
+				)
+			],
+			'created_at:datetime',
+			'updated_at:datetime',
+			'createdBy.username',
+			'updatedBy.username',
+		],
+	]) ?>
 
 </div>
